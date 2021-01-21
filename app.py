ae80f9d7cff362c9a9856d99699b4ea76faef861
john-montgomery2003 <johnmontgomery2003@gmail.com>
2021-01-21 13:18:35 +0000

    ls

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+rows = []
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return '<h1> Hello World</h1>'
+#    return render_template('index.html', names=rows)
+
+# @app.route('/process_name', methods = ['POST'])
+# def process_name():
+#     name =request.form['name']
+#     age=request.form['age']
+#     rows.append([name,age])
+#     print("Recieved request to add " + request.form["name"])
+#
+#     return '<h1>' + request.form["name"] + '</h1>'
+# #    return render_template('index.html', names=rows)
 
 
-if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 80)
+if __name__ == "__main__":
+    app.run()
